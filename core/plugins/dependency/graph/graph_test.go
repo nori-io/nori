c7232272cb920b727017fde51da148ba789ccacb
anita <bruteforce1414@gmail.com>
2019-01-29 11:21:39 +0500

    Add testing for plugins in dependencies (second and third set of data), test passed

--- core/plugins/dependency/graph/graph_test.go
+++ core/plugins/dependency/graph/graph_test.go
@@ -7,9 +7,11 @@ import (
 	"testing"
 )
 
-func TestDependencyGraph_Sort(t *testing.T) {
+//1) плагин-1 -> плагин-2 -> плагин-3 (все в наличии)
+/*func TestDependencyGraph_Sort1(t *testing.T) {
 	a := assert.New(t)
 	managerPlugin := dependency.NewManager()
+
 	plugin1 := meta.Data{
 		ID: meta.ID{
 			ID:      "plugin1",
@@ -93,7 +95,6 @@ func TestDependencyGraph_Sort(t *testing.T) {
 	managerPlugin.Add(plugin3)
 
 	t.Log("Порядок плагинов до сортировки:", managerPlugin)
-
 	pluginsSorted, err := managerPlugin.Sort()
 	if err != nil {
 		t.Log("Erroe in sorting")
@@ -122,5 +123,201 @@ func TestDependencyGraph_Sort(t *testing.T) {
 
 	a.Equal(true, index3 < index2)
 	a.Equal(true, index2 < index1)
+	managerPlugin.Remove(plugin1.ID)
+	managerPlugin.Remove(plugin2.ID)
+	managerPlugin.Remove(plugin3.ID)
+
+}*/
+
+//2) плагин-1 -> плагин-2 -> плагин-3 (3й отсутствует)
+/*func TestDependencyGraph_Sort2(t *testing.T) {
+	a := assert.New(t)
+	managerPlugin := dependency.NewManager()
+
+	//
+	plugin1 := meta.Data{
+		ID: meta.ID{
+			ID:      "plugin1",
+			Version: "1.0",
+		},
+		Author: meta.Author{
+			Name: "NoriCMS",
+			URI:  "https://noricms.com",
+		},
+		Core: meta.Core{
+			VersionConstraint: ">=1.0, <2.0",
+		},
+		Dependencies: []meta.Dependency{
+			{"plugin2", ">=1.0, <2.0", meta.Custom},
+		},
+		Description: meta.Description{
+			Name: "Nori HTTP Interface",
+		},
+		Interface: meta.Custom,
+		License: meta.License{
+			Title: "",
+			Type:  "GPLv3",
+			URI:   "https://www.gnu.org/licenses/"},
+		Tags: []string{"http"},
+	}
+
+	plugin2 := meta.Data{
+		ID: meta.ID{
+			ID:      "plugin2",
+			Version: "1.0",
+		},
+		Author: meta.Author{
+			Name: "NoriCMS",
+			URI:  "https://noricms.com",
+		},
+		Core: meta.Core{
+			VersionConstraint: ">=1.0, <2.0",
+		},
+		Dependencies: []meta.Dependency{
+			{"plugin3", ">=1.0, <2.0", meta.Custom},
+		},
+		Description: meta.Description{
+			Name: "NoriCMS: MySQL Driver",
+		},
+		Interface: meta.Custom,
+		License: meta.License{
+			Title: "",
+			Type:  "GPLv3",
+			URI:   "https://www.gnu.org/licenses/"},
+		Tags: []string{"sql", "mysql"},
+	}
+	managerPlugin.Add(plugin1)
+	managerPlugin.Add(plugin2)
+
+
+	t.Log("Порядок плагинов до сортировки:", managerPlugin)
+	pluginsSorted, err := managerPlugin.Sort()
+	if err != nil {
+		t.Log("Erroe in sorting")
+	}
+	for index, _ := range pluginsSorted {
+		t.Log(index+1, "-й элемент для запуска", pluginsSorted[index].ID, " ", pluginsSorted[index].Version)
+	}
+
+	var (
+		index1 int
+		index2 int
+		index3 int
+	)
+	for index, value := range pluginsSorted {
+		if value.ID == "plugin1" {
+			index1 = index
+		}
+
+		if value.ID == "plugin2" {
+			index2 = index
+		}
+		if value.ID == "plugin3" {
+			index3 = index
+		}
+	}
+	//index3 initialized by default value=0
+	a.Equal(true, index3==0)
+	a.Equal(true, index2 < index1)
+	managerPlugin.Remove(plugin1.ID)
+	managerPlugin.Remove(plugin2.ID)
+
+}
+*/
+
+//3) плагин-1 -> плагин-2 -> плагин-3 (2й отсутствует)
+func TestDependencyGraph_Sort3(t *testing.T) {
+	a := assert.New(t)
+	managerPlugin := dependency.NewManager()
+
+	plugin1 := meta.Data{
+		ID: meta.ID{
+			ID:      "plugin1",
+			Version: "1.0",
+		},
+		Author: meta.Author{
+			Name: "NoriCMS",
+			URI:  "https://noricms.com",
+		},
+		Core: meta.Core{
+			VersionConstraint: ">=1.0, <2.0",
+		},
+		Dependencies: []meta.Dependency{
+			{"plugin2", ">=1.0, <2.0", meta.Custom},
+		},
+		Description: meta.Description{
+			Name: "Nori HTTP Interface",
+		},
+		Interface: meta.Custom,
+		License: meta.License{
+			Title: "",
+			Type:  "GPLv3",
+			URI:   "https://www.gnu.org/licenses/"},
+		Tags: []string{"http"},
+	}
+
+
+	plugin3 := meta.Data{
+		ID: meta.ID{
+			ID:      "plugin3",
+			Version: "1.0",
+		},
+		Author: meta.Author{
+			Name: "Nori",
+			URI:  "https://noricms.com",
+		},
+		Core: meta.Core{
+			VersionConstraint: ">=1.0, <2.0",
+		},
+		Dependencies: []meta.Dependency{},
+		Description: meta.Description{
+			Name:        "NoriCMS Naive Posts Plugin",
+			Description: "Naive Posts Plugin",
+		},
+		Interface: meta.Custom,
+		License: meta.License{
+			Title: "",
+			Type:  "GPLv3",
+			URI:   "https://www.gnu.org/licenses/",
+		},
+		Tags: []string{"cms", "posts", "api"},
+	}
+
+
+	managerPlugin.Add(plugin1)
+	managerPlugin.Add(plugin3)
+
+	t.Log("Порядок плагинов до сортировки:", managerPlugin)
+	pluginsSorted, err := managerPlugin.Sort()
+	if err != nil {
+		t.Log("Erroe in sorting")
+	}
+	for index, _ := range pluginsSorted {
+		t.Log(index+1, "-й элемент для запуска", pluginsSorted[index].ID, " ", pluginsSorted[index].Version)
+	}
+
+	var (
+		index1 int
+		index2 int
+		index3 int
+	)
+	for index, value := range pluginsSorted {
+		if value.ID == "plugin1" {
+			index1 = index
+		}
+
+		if value.ID == "plugin2" {
+			index2 = index
+		}
+		if value.ID == "plugin3" {
+			index3 = index
+		}
+	}
+
+	a.Equal(true, index3 < index1||index3>index1)
+	//index2 initialized by default value=0
+	a.Equal(true, index2==0)
+	managerPlugin.Remove(plugin1.ID)
+	managerPlugin.Remove(plugin3.ID)
 
 }
